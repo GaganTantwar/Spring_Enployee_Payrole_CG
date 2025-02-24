414b393f08d40a5eb05947762916e75372038d86
GaganTantwar <panthigagan478@gmail.com>
2025-02-24 19:43:44 +0530

    Gagan Tantwar Extended feat :Create a Rest Controller to demonstrate the various HTTP

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeRestController.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.employeepayrollapp.controller;
+import com.bridgelabz.employeepayrollapp.model.Employee;
+import com.bridgelabz.employeepayrollapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+public class EmployeeRestController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+            this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+    @PatchMapping("/{id}/salary")
+    public Employee updateEmployeeSalary(@PathVariable Long id, @RequestParam double salary) {
+        Optional<Employee> employeeOptional = employeeService.getEmployeeById(id);
+        if (employeeOptional.isPresent()) {
+            Employee employee = employeeOptional.get();
+            employee.setSalary(salary);
+            return employeeService.saveEmployee(employee);
+        }
+        return null;
+    }
+}
+
